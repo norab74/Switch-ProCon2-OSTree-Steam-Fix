ac557c305de10e8dcadfc8e4b49ac707213b4086
norab74 <noranbbell@gmail.com>
2026-03-02 11:01:54 -0700

    Remove ostree admin unlock command

--- fixSwitch2ProCon.sh
+++ fixSwitch2ProCon.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-ostree admin unlock
 echo -e '\n SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="2069", MODE="0660", TAG+="uaccess"' | tee -a /usr/lib/udev/rules.d/60-steam-input.rules
